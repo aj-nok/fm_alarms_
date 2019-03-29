3ddc3fc1bface2e54abbc60f9574d90ca3c48139
aj-nok <47238495+aj-nok@users.noreply.github.com>
2019-03-29 11:07:58 +0530

    Update data_visualization_code.py

--- data_visualization_code.py
+++ data_visualization_code.py
@@ -11,18 +11,10 @@ import matplotlib.dates as mdates
 
 
 data = pd.read_csv('fm_alarms_18012019_085951.csv', usecols=['CONSEC_NBR','EVENT_TIME'], parse_dates=['EVENT_TIME'])
-data=data.sort_values('CONSEC_NBR')
-data.set_index('EVENT_TIME',inplace=True)
+                                                                   #importing csv and parsing date fields
 
-#data1 = pd.read_csv('fm_alarms_18012019_085951.csv', usecols=['INSERT_TIME','CONSEC_NBR'], parse_dates=['INSERT_TIME'])
-#data1=data1.sort_values('CONSEC_NBR')
-#data1.set_index('INSERT_TIME',inplace=True)
-
+  
+data.set_index('EVENT_TIME',inplace=True)                          #plotting date
 fig, ax = plt.subplots(figsize=(25,8))
 data.plot(ax=ax)
-#ax.yaxis_date()
-#data1.plot(ax=ax)
 
-
-#ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-#ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
